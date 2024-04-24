b84b9877a5e47fd921a28b81bfa6e6fa9a0fa7d3
Josh Dickson <joshdickson40@gmail.com>
2024-04-24 10:27:56 -0400

    debug

--- examples/expo/db/migrations/01-create_items_table.sql
+++ examples/expo/db/migrations/01-create_items_table.sql
@@ -1,19 +1,106 @@
-/* This is an example of an SQL DDL migration. It creates an `items` table and
- * then calls an `electric.electrify` procedure to expose the table to the
- * ElectricSQL replication machinery.
- *
- * Note that these statements are applied directly to the *Postgres* database.
- * Electric then handles keeping the local SQLite database schema in sync with
- * the electrified subset of your Postgres database schema.
- *
- * See https://electric-sql.com/docs/usage/data-modelling for more information.
- */
+-- general functions
 
--- Create a simple items table.
-CREATE TABLE IF NOT EXISTS items (
-  value TEXT PRIMARY KEY NOT NULL
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Check if the new row is different from the old row
+    IF ROW(NEW.*) IS DISTINCT FROM ROW(OLD.*) THEN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    ELSE
+        RETURN OLD;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- tables
+
+CREATE TABLE IF NOT EXISTS "brands" (
+    id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    mf_id TEXT NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "brands_pkey" PRIMARY KEY ("id")
 );
 
--- ⚡
--- Electrify the items table
-ALTER TABLE items ENABLE ELECTRIC;
+CREATE TRIGGER update_brands_modtime
+BEFORE UPDATE ON brands
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+
+CREATE TABLE IF NOT EXISTS "foods" (
+    id TEXT NOT NULL,
+
+    name TEXT NOT NULL,
+    alternate_names TEXT, -- alternate names of the food
+    -- tags VARCHAR(128)[] DEFAULT ARRAY[]::VARCHAR(128)[], -- todo, does not work with Electric
+    
+    type TEXT NOT NULL, -- whole, manufactured, branded
+    category TEXT, -- the category of the food
+    
+    brand_id TEXT, -- the brand of the food
+    ean_13 TEXT, -- the EAN-13 barcode of the food
+
+    image_slug TEXT, -- the slug of the image
+    image_source TEXT, -- the source of the image
+    
+    serving_quantity_common DOUBLE PRECISION NOT NULL,
+    serving_unit_common TEXT NOT NULL,
+    serving_quantity_metric DOUBLE PRECISION NOT NULL,
+    serving_unit_metric TEXT NOT NULL,
+
+    nutrition JSONB NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+
+    FOREIGN KEY ("brand_id") REFERENCES "brands" ("id"),
+    CONSTRAINT "foods_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TRIGGER update_foods_modtime
+BEFORE UPDATE ON foods
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+
+CREATE TABLE IF NOT EXISTS "entries" (
+    id TEXT NOT NULL,
+
+    "user_id" TEXT NOT NULL,
+    "food_id" TEXT, -- the associated food, if one is linked
+
+    name TEXT NOT NULL, -- the human-readable name of the entry
+
+    serving_quantity_common DOUBLE PRECISION NOT NULL,
+    serving_unit_common TEXT NOT NULL,
+    serving_quantity_metric DOUBLE PRECISION NOT NULL,
+    serving_unit_metric TEXT NOT NULL,
+
+    date TIMESTAMPTZ NOT NULL,
+
+    nutrition JSONB NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+
+    FOREIGN KEY ("food_id") REFERENCES "foods" ("id"),
+
+    CONSTRAINT "entries_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TRIGGER update_entries_modtime
+BEFORE UPDATE ON entries
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+-- electrify tables
+
+ALTER TABLE foods ENABLE ELECTRIC;
+ALTER TABLE brands ENABLE ELECTRIC;
+ALTER TABLE entries ENABLE ELECTRIC;
--- examples/expo/src/Example.tsx
+++ examples/expo/src/Example.tsx
@@ -1,46 +1,59 @@
-import React, { useEffect } from 'react'
-import { Image, Pressable, Text, View } from 'react-native'
-import { useLiveQuery } from 'electric-sql/react'
-import { genUUID } from 'electric-sql/util'
-import { Items as Item } from './generated/client'
+import { useLiveQuery } from "electric-sql/react";
+import React, { useEffect } from "react";
+import { Image, Pressable, Text, View } from "react-native";
+import { Foods as Food } from "./generated/client";
 
-import { useElectric } from './ElectricProvider'
-import { styles } from './styles'
+import { useElectric } from "./ElectricProvider";
+import { styles } from "./styles";
 
 export const Example = () => {
-  const { db } = useElectric()!
-  const { results } = useLiveQuery(db.items.liveMany())
+  const { db } = useElectric()!;
+  const { results } = useLiveQuery(db.foods.liveMany());
 
   useEffect(() => {
     const syncItems = async () => {
       // Resolves when the shape subscription has been established.
-      const shape = await db.items.sync()
+      const shape = await db.foods.sync();
 
       // Resolves when the data has been synced into the local database.
-      await shape.synced
-    }
+      await shape.synced;
+    };
 
-    syncItems()
-  }, [])
+    syncItems();
+  }, []);
 
   const addItem = async () => {
-    await db.items.create({
+    await db.foods.create({
       data: {
-        value: genUUID(),
+        id: "id_001",
+        name: "Name",
+        type: "Type",
+        serving_quantity_common: 1,
+        serving_unit_common: "unit",
+        serving_quantity_metric: 1,
+        serving_unit_metric: "g",
+        nutrition: {
+          calories: 100,
+          protein: 10,
+          fat: 10,
+          carbs: 10,
+        },
+        created_at: new Date(),
+        updated_at: new Date(),
       },
-    })
-  }
+    });
+  };
 
   const clearItems = async () => {
-    await db.items.deleteMany()
-  }
+    await db.foods.deleteMany();
+  };
 
-  const items: Item[] = results ?? []
+  const items: Food[] = results ?? [];
 
   return (
     <View>
       <View style={styles.iconContainer}>
-        <Image source={require('../assets/icon.png')} />
+        <Image source={require("../assets/icon.png")} />
       </View>
       <View style={styles.buttons}>
         <Pressable style={styles.button} onPress={addItem}>
@@ -51,12 +64,12 @@ export const Example = () => {
         </Pressable>
       </View>
       <View style={styles.items}>
-        {items.map((item: Item, index: number) => (
+        {items.map((item: Food, index: number) => (
           <Text key={index} style={styles.item}>
-            {item.value}
+            {item.name}
           </Text>
         ))}
       </View>
     </View>
-  )
-}
+  );
+};
