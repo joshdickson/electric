59b0a27c6fec300d721f1221bae00613bc5f96e2
Josh Dickson <joshdickson40@gmail.com>
2024-04-26 14:25:33 -0400

    simplify demo

--- examples/expo/src/ElectricProvider.tsx
+++ examples/expo/src/ElectricProvider.tsx
@@ -1,54 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { authToken } from './auth'
-import { DEBUG_MODE, ELECTRIC_URL } from './config'
-import { Electric, schema } from './generated/client'
+import React, { useEffect, useState } from "react";
+import { authToken } from "./auth";
+import { DEBUG_MODE, ELECTRIC_URL } from "./config";
+import { Electric, schema } from "./generated/client";
 
-import * as SQLite from 'expo-sqlite/next'
+import * as SQLite from "expo-sqlite/next";
 
-import { electrify } from 'electric-sql/expo-next'
-import { makeElectricContext } from 'electric-sql/react'
+import { electrify } from "electric-sql/expo-next";
+import { makeElectricContext } from "electric-sql/react";
 
-const { ElectricProvider, useElectric } = makeElectricContext<Electric>()
+const { ElectricProvider, useElectric } = makeElectricContext<Electric>();
 
-const ElectricProviderComponent = ({
-  children,
-}: {
-  children: React.ReactNode
-}) => {
-  const [electric, setElectric] = useState<Electric>()
+const ElectricProviderComponent = ({ children }: { children: React.ReactNode }) => {
+  const [electric, setElectric] = useState<Electric>();
 
   useEffect(() => {
-    let isMounted = true
+    let isMounted = true;
 
     const init = async () => {
       const config = {
         debug: DEBUG_MODE,
         url: ELECTRIC_URL,
-      }
+      };
 
-      const conn = SQLite.openDatabaseSync('electric.db')
-      const client = await electrify(conn, schema, config)
-      await client.connect(authToken())
+      const conn = SQLite.openDatabaseSync("integer-test.db");
+      const client = await electrify(conn, schema, config);
+      await client.connect(authToken());
 
       if (!isMounted) {
-        return
+        return;
       }
 
-      setElectric(client)
-    }
+      setElectric(client);
+    };
 
-    init()
+    init();
 
     return () => {
-      isMounted = false
-    }
-  }, [])
+      isMounted = false;
+    };
+  }, []);
 
   if (electric === undefined) {
-    return null
+    return null;
   }
 
-  return <ElectricProvider db={electric}>{children}</ElectricProvider>
-}
+  return <ElectricProvider db={electric}>{children}</ElectricProvider>;
+};
 
-export { ElectricProviderComponent as ElectricProvider, useElectric }
+export { ElectricProviderComponent as ElectricProvider, useElectric };
--- examples/expo/src/Example.tsx
+++ examples/expo/src/Example.tsx
@@ -1,19 +1,19 @@
 import { useLiveQuery } from "electric-sql/react";
 import React, { useEffect } from "react";
 import { Image, Pressable, Text, View } from "react-native";
-import { Foods as Food } from "./generated/client";
+import { Entries as Entry } from "./generated/client";
 
 import { useElectric } from "./ElectricProvider";
 import { styles } from "./styles";
 
 export const Example = () => {
   const { db } = useElectric()!;
-  const { results } = useLiveQuery(db.foods.liveMany());
+  const { results } = useLiveQuery(db.entries.liveMany());
 
   useEffect(() => {
     const syncItems = async () => {
       // Resolves when the shape subscription has been established.
-      const shape = await db.foods.sync();
+      const shape = await db.entries.sync();
 
       // Resolves when the data has been synced into the local database.
       await shape.synced;
@@ -23,32 +23,34 @@ export const Example = () => {
   }, []);
 
   const addItem = async () => {
-    await db.foods.create({
+    await db.entries.create({
       data: {
-        id: "id_001",
-        name: "Name",
-        type: "Type",
-        serving_quantity_common: 1,
-        serving_unit_common: "unit",
-        serving_quantity_metric: 1,
-        serving_unit_metric: "g",
-        nutrition: {
-          calories: 100,
-          protein: 10,
-          fat: 10,
-          carbs: 10,
-        },
-        created_at: new Date(),
-        updated_at: new Date(),
+        id: "id_002",
+        // name: "Name",
+        // user_id: "user_id_001",
+        // date: new Date(),
+        // type: "Type",
+        serving: 1,
+        // serving_unit_common: "unit",
+        // serving_quantity_metric: 1,
+        // serving_unit_metric: "g",
+        // nutrition: {
+        //   calories: 100,
+        //   protein: 10,
+        //   fat: 10,
+        //   carbs: 10,
+        // },
+        // created_at: new Date(),
+        // updated_at: new Date(),
       },
     });
   };
 
   const clearItems = async () => {
-    await db.foods.deleteMany();
+    await db.entries.deleteMany();
   };
 
-  const items: Food[] = results ?? [];
+  const items: Entry[] = results ?? [];
 
   return (
     <View>
@@ -64,9 +66,9 @@ export const Example = () => {
         </Pressable>
       </View>
       <View style={styles.items}>
-        {items.map((item: Food, index: number) => (
+        {items.map((item: Entry, index: number) => (
           <Text key={index} style={styles.item}>
-            {item.name}
+            {item.id}
           </Text>
         ))}
       </View>
